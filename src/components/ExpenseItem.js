96e6df55df02c0833d0424e28c1ac6f3aed7adaf
daphneruth <daphneruth304@gmail.com>
2022-04-22 20:01:46 +0300

    updated

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,11 @@
 function ExpenseItem() {
-    return <h2>Expense Item !</h2>
+  return (
+    <div>
+      <div>4/22/2022</div>
+      <div>Car Insurance</div>
+      <div>$300</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
