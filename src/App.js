b5bd8459504f64e37b2635e848457fb698e57b52
daphneruth <daphneruth304@gmail.com>
2022-04-23 12:47:39 +0300

    styling item component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ExpenseItem from './components/ExpenseItem';
 function App() {
   return (
   
-   <div className="App">
+ <div className="App">
       
     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
