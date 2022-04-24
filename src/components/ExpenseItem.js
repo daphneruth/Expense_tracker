183081bda67517440d2b8af14428af2bf1cdc5bc
daphneruth <daphneruth304@gmail.com>
2022-04-24 13:17:36 +0300

    reafactoring date format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,9 +5,17 @@ function ExpenseItem(props) {
   //const expenseDate = new Date(2022, 3, 24);
   //const expenseName = "Car Insurance";
   //const expensePrice = 300;
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const year = props.date.toLocaleString('en-US', {day: '2-digit'});
+  const day = props.date.getFullYear();
 
   return (
     <div className="expense-item">
+      <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+      </div>
       <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2></div>
