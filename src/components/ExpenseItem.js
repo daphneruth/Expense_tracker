2ae832e11fae75a7bef3dc9943641a9b4af5a6b5
daphneruth <daphneruth304@gmail.com>
2022-04-24 11:54:41 +0300

    outputing data dynamically

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import '../components/ExpenseStyling.css';
 
 function ExpenseItem() {
+
+  const expenseDate = new Date(2022, 3, 24);
+  const expenseName = "Car Insurance";
+  const expensePrice = 300;
   return (
     <div className="expense-item">
-      <div>4/22/2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2></div>
-      <div className="expense-item__price">$300</div>
+        <h2>{expenseName}</h2></div>
+      <div className="expense-item__price">${expensePrice}</div>
     </div>
   );
 }
