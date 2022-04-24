1e173d15fb0f09fb498cdde9f5181769b90aaa7c
daphneruth <daphneruth304@gmail.com>
2022-04-24 13:00:14 +0300

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expense = [
-    { title: CarInsurance, amount: 300, date: new Date(2022, 4, 24) },
-    { title: HealthInsurance, amount: 450, date: new Date(2022, 5, 25) },
-    { title: Housensurance, amount: 200, date: new Date(2022, 7, 27) },
-    { title: GeneralInsurance, amount: 600, date: new Date(2023, 9, 30) },
+    { title:" Car Insurance", amount: 300, date: new Date(2022, 4, 24) },
+    { title: "Health Insurance", amount: 450, date: new Date(2022, 5, 25) },
+    { title: "House Insurance", amount: 200, date: new Date(2022, 7, 27) },
+    { title: "General Insurance", amount: 600, date: new Date(2023, 9, 30) },
   ];
 
   return (
@@ -43,11 +43,13 @@ function App() {
         amount={expense[1].amount}
         date={expense[1].date}
       ></ExpenseItem>
+
       <ExpenseItem
         title={expense[2].title}
         amount={expense[2].amount}
         date={expense[2].date}
       ></ExpenseItem>
+
       <ExpenseItem
         title={expense[3].title}
         amount={expense[3].amount}
