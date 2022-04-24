90c9c2ee289166713ba37b1b8491eb8c4e1368c4
daphneruth <daphneruth304@gmail.com>
2022-04-24 11:42:16 +0300

    fixed some bugs

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,7 +4,8 @@ function ExpenseItem() {
   return (
     <div className="expense-item">
       <div>4/22/2022</div>
-      <div className="expense-item__description">Car Insurance</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2></div>
       <div className="expense-item__price">$300</div>
     </div>
   );
