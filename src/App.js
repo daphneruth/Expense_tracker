36456f9e24b0394f79c3477047b8506ba051431c
daphneruth <daphneruth304@gmail.com>
2022-04-24 11:11:38 +0300

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
+
 function App() {
   return (
     
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,11 @@
-
+import '../components/ExpenseStyling.css';
 
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>4/22/2022</div>
-      <div className=".expense-item">Car Insurance</div>
-      <div className=".expense-item__price">$300</div>
+      <div className="expense-item__description">Car Insurance</div>
+      <div className="expense-item__price">$300</div>
     </div>
   );
 }
