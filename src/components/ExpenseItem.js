e06bf3b0921d58df8b50c96b11f7ba9b8e1e5303
daphneruth <daphneruth304@gmail.com>
2022-04-24 13:29:45 +0300

    spliting components
    spiting date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,14 @@
 import '../components/ExpenseStyling.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
-  //const expenseDate = new Date(2022, 3, 24);
-  //const expenseName = "Car Insurance";
-  //const expensePrice = 300;
-  const month = props.date.toLocaleString('en-US', {month: 'long'});
-  const year = props.date.toLocaleString('en-US', {day: '2-digit'});
-  const day = props.date.getFullYear();
+
 
   return (
     <div className="expense-item">
-      <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
-      </div>
-      
+
+      <ExpenseDate></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2></div>
       <div className="expense-item__price">${props.amount}</div>
