f8da3b2f318718a7fc5c21a3dfcf26a4d557392a
daphneruth <daphneruth304@gmail.com>
2022-04-26 23:19:49 +0300

    refactored the code by creating new components

--- src/App.js
+++ src/App.js
@@ -11,7 +11,14 @@ function App() {
     { title: "General Insurance", amount: 600, date: new Date(2023, 9, 30) },
   ];
 
-  return (
+  return(
+    <div>
+      <Expenses expensesData={expenses}/>
+    </div>
+);
+}
+
+  
     /*<div className="App">
       
     <header className="App-header">
@@ -31,32 +38,6 @@ function App() {
       </div>
       */
 
-    <div>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
 
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
-    </div>
-  );
-}
 
 export default App;
---
+++ src/components/Expenses.js
@@ -0,0 +1,31 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(expensesProps){
+ return(
+    <div>
+      <ExpenseItem
+        title={expensesProps.expensesData[0].title}
+        amount={expensesProps.expensesData[0].amount}
+        date={expensesProps.expensesData[0].date}
+      />
+      <ExpenseItem
+        title={expensesProps.expensesData[1].title}
+        amount={expensesProps.expensesData[1].amount}
+        date={expensesProps.expensesData[1].date}
+      />
+      <ExpenseItem
+        title={expensesProps.expensesData[2].title}
+        amount={expensesProps.expensesData[2].amount}
+        date={expensesProps.expensesData[2].date}
+      />
+      <ExpenseItem
+        title={expensesProps.expensesData[3].title}
+        amount={expensesProps.expensesData[3].amount}
+        date={expensesProps.expensesData[3].date}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
