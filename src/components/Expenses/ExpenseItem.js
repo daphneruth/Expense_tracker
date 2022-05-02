abb54db05ed5d885ef44b4eaa728ccd92c61a8f5
daphneruth <daphneruth304@gmail.com>
2022-05-02 12:52:03 +0300

    working with events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -19,7 +19,7 @@ function ExpenseItem(props) {
       <div className="expense-item__description">
         <h2>{props.title}</h2></div>
       <div className="expense-item__price">${props.amount}</div>
-
+     <button>Change Title</button>
       
     </Card>
 
