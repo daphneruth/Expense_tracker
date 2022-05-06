7b18602a5e055063f7750a44ee7c704d025260c2
daphneruth <daphneruth304@gmail.com>
2022-05-06 09:18:14 +0300

    handling input events

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    const changeHandler = (event) => (
+        console.log(event.target.value)
+    )
   return (
     <form>
       <div className="new-expense__controls ">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={changeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
