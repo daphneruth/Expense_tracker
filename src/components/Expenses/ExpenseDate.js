2183fc6300d3b075f5bd8f2c5760731193bb56b3
daphneruth <daphneruth304@gmail.com>
2022-05-10 20:27:07 +0300

    updated

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -5,8 +5,8 @@ import './ExpenseDate.css';
 
 const ExpenseDate = (props) => 
 {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const year = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const month = props.date.toLocaleString("en-US", { month: "long" }) ;
+  const year = props.date.toLocaleString("en-US", { day: "2-digit" }) ;
   const day = props.date.getFullYear();
 
   return (
