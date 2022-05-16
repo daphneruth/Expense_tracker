f9c038ca1678eef83ce778a281b74c843155531b
daphneruth <daphneruth304@gmail.com>
2022-05-16 22:27:51 +0300

    fixed some bugs

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -6,13 +6,14 @@ import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
 
-    const saveExpenseDataHandler = (enteredExpenseData)
+    const saveExpenseDataHandler = (enteredExpenseData) =>{
     
     const expenseData = {
        ...enteredExpenseData,
         id:Math.random().toString()
     };
 props.onAddExpense(expenseData)
+    }
 
   
     return  <div className='new-expense'>
