1b748b7bba2a0075db7e01673c76ad0602d1636a
daphneruth <daphneruth304@gmail.com>
2022-05-18 08:40:43 +0300

    updated

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -25,28 +25,8 @@ const Expenses = (Props) => {
       date = {expense.date}
       />
     ))}
+     
       
-
-      <ExpenseItem
-        title={Props.data[0].title}
-        amount={Props.data[0].amount}
-        date={Props.data[0].date}
-      />
-      <ExpenseItem
-        title={Props.data[1].title}
-        amount={Props.data[1].amount}
-        date={Props.data[1].date}
-      />
-      <ExpenseItem
-        title={Props.data[2].title}
-        amount={Props.data[2].amount}
-        date={Props.data[2].date}
-      />
-      <ExpenseItem
-        title={Props.data[3].title}
-        amount={Props.data[3].amount}
-        date={Props.data[3].date}
-      />
       </Card>
       </div>
     
