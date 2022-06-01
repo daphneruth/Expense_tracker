4bb51bcbad97e2bd96e5bc700871f9c019039d04
daphneruth <daphneruth304@gmail.com>
2022-06-01 16:46:28 +0300

    fixed some bug

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Chart from "../Chart/Chart";
- const ExpensesChart = () => {
+ const ExpensesChart = (props) => {
      const chartDataPoints = [
          {label : 'Jan', value: 0},
          {label : 'Feb', value: 0},
