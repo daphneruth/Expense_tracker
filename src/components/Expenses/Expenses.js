c2b61de1b96583700aabcad23d9913deae30d4cd
daphneruth <daphneruth304@gmail.com>
2022-06-01 16:37:48 +0300

    rendering expensechart in expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpenseList from "./ExpenseList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setfilteredYear] = useState("2021");
@@ -24,6 +25,7 @@ const Expenses = (props) => {
             selected={filteredYear}
             onChangeFilter={filterChangeHandler}
           />
+          <ExpensesChart/>
 
           <ExpenseList items={filteredExpenses} />
         </Card>
