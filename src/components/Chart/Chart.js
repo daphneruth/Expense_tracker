d0de1282763ea6da882039386a6c418db317f909
daphneruth <daphneruth304@gmail.com>
2022-06-01 16:31:26 +0300

    finding the max value

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,14 +4,14 @@ import ChartBar from "./ChartBar";
 
 const Chart = (props) => {
     const dataPointValues = props.dataPoints.map(dataPoint =>dataPoint.value)
-    const totalMax = Math.max();
+    const totalMax = Math.max(...dataPointValues);
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key ={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMax}
           label={dataPoint.label}
         />
       ))}
