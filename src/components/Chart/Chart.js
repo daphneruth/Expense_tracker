4ac76c7100be4b4e4b5ca972bd04e73a12103070
daphneruth <daphneruth304@gmail.com>
2022-06-03 10:59:42 +0300

    fixed some bugs

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,8 +3,9 @@ import "./Chart.css";
 import ChartBar from "./ChartBar";
 
 const Chart = (props) => {
-    const dataPointValues = props.dataPoints.map(dataPoint =>dataPoint.value)
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
     const totalMax = Math.max(...dataPointValues);
+    
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./ChartBar.css";
 
 const ChartBar = (props) => {
-  let barFillHeight = "0%";
+  let barFillHeight = '0%';
   if (props.maxValue > 0) {
-    barFillHeight = Math.round((props.value / props.maxValue) * 100 + "%");
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
 
   return (
